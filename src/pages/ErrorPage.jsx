74c1fab783377e62050f3f5aa5d23d2d6efb124d
Alamin4D <alaminahmed79000@gmail.com>
2024-05-12 22:39:32 +0600

    added the error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
     return (
-        <section className='bg-white '>
+        <section className='bg-white'>
+            {/* error page */}
             <div className='container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12'>
                 <div className='wf-ull lg:w-1/2'>
                     <p className='text-sm font-medium text-gray-500'>404 error</p>
